624d9ca72bef7e96ac172c5b3630f172f20e819a
Adam <adamk007pa@gmail.com>
2025-09-07 14:30:23 -0700

    Add new Views and recreate ContentView

---
+++ WorkoutTimer/Root/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  WorkoutTimer
+//
+//  Created by Adam Dotm on 07.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutTimer/Root/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  WorkoutTimer
+//
+//  Created by Adam Dotm on 07.09.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+        
+    }
+}
+
+#Preview {
+    MainView()
+}
