6364227f743049c785b3bc8a5ad9ba5144f30b8f
Adam <adamk007pa@gmail.com>
2025-09-07 14:08:13 -0700

    Initial Commit

---
+++ WorkoutTimer/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer
+//
+//  Created by Adam Dotm on 07.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
